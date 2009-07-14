03845d27addbe56e4ff2d20723f9745be638390f
goncalopp <goncalopp@will.(none)>
2009-07-14 01:16:44 +0100

    updated make.sh to include circularbuffers.c

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wall -o strebupitecha main.c `pkg-config --cflags --libs gtk+-2.0 jack` -export-dynamic
+gcc -Wall -o strebupitecha main.c circularbuffers.c `pkg-config --cflags --libs gtk+-2.0 jack rubberband` -export-dynamic
 
